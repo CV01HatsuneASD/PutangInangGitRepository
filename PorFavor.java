01cc87b4b32cc4eb799f91c6bbecffa591c3a71b
Daniel Nicolas <40626713+CV01HatsuneASD@users.noreply.github.com>
2020-01-24 20:46:24 +0800

    Uploaded PorFavor.java
    ¿Por favor?

---
+++ PorFavor.java
@@ -0,0 +1,16 @@
+/**
+ * James: Bobo ka Daniel!! Wala kang pakialam sa ginagawa ko!! Inggit ka lang;
+ * matalino ako!! Baka 'di mo nalalaman na ako ang pinakamagaling mag-code
+ * noon pang Senior High School dito sa STI Caloocan!!
+ * 
+ * Me:
+ */
+
+ public class PorFavor {
+     public static void main(final String[]args) {
+         final String James = "madaldal";
+         do {
+             System.out.println("¿Por favor?");
+         } while(James == "madaldal");
+     }
+ }
